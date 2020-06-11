f88c67aca259e2e1f0e4c05f2fd32297ca11ce73
Fairy <Fairy_Lin@wistron.com>
2020-06-11 14:11:41 +0800

    v1.1

--- src/app/components/activity/activity.component.ts
+++ src/app/components/activity/activity.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var layui;
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
@@ -10,6 +11,9 @@ export class ActivityComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    layui.use('element', function(){
+      var element = layui.element;
+    });
   }
 
 }
